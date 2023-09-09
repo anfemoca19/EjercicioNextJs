03b63948580927556d5e7cbb068dcc2af0cf3c34
Andres Morales <appandres993@gmail.com>
2023-09-09 17:56:23 -0500

    Arreglo los botones

--- src/components/userList/userList.tsx
+++ src/components/userList/userList.tsx
@@ -23,8 +23,8 @@ const UserList: React.FC<UserListProps> = ({ users, onEdit, onDelete }) => {
             <p className='text-center '>Nombre: {user.name}</p>
             <p className='text-center'>Correo Electrónico: {user.email}</p>
             <p className='text-center'>Telefono : {user.phone}</p>
-            <div className='flex justify-center'>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => onEdit(user)}>Editar</button>
+            <div className='flex justify-center mt-5'>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2" onClick={() => onEdit(user)}>Editar</button>
               <button className="bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => onDelete(user.id)}>Eliminar</button>
             </div>
             
