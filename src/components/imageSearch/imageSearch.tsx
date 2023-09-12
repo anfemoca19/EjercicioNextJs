dcbe9d3217faf1f9e7c9b63e9dbc1e51cb9fb79b
Andres Morales <appandres993@gmail.com>
2023-09-11 22:33:46 -0500

    Manejo de error cuando dan buscar sin datos

--- src/components/imageSearch/imageSearch.tsx
+++ src/components/imageSearch/imageSearch.tsx
@@ -6,16 +6,25 @@ type ImageSearchProps = {
 
 const ImageSearch: React.FC<ImageSearchProps> = ({ onSearch }) => {
   const [query, setQuery] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    debugger
+    if (query.trim() === '') {
+      setError('Por favor, ingresa un término de búsqueda válido.');
+      return;
+    }
+
+    setError('');
     onSearch(query);
   };
+
 
   return (
     <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
       <form onSubmit={handleSubmit} className='w-full max-w-sm'>
-        <div className='flex items-center border-b border-b-2 border-grey-500 py-2'>
+        <div className='flex items-center border-b-2 border-grey-500 py-2'>
           <input
             type="text"
             placeholder="Buscar imágenes..."
@@ -25,8 +34,10 @@ const ImageSearch: React.FC<ImageSearchProps> = ({ onSearch }) => {
           />
           <button type="submit" className='flex-shrink-0 bg-grey-500 hover:bg-grey-700 border-grey-500 
           hover:border-grey-700 text-sm border-4 py-1 px-2 rounded'>Buscar</button>
+          {
+              error && <span className='text-red-200'>{error}</span>
+          }
         </div>
-      
       </form>
     </div>
       
